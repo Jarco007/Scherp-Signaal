52fdf3e99e9f53dd1c79b60bdf2cb5aae6138ea4
Jarco <jarco91@gmail.com>
2026-06-11 11:37:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'scherp-signaal-v4-2';
+const URLS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      const clone = r.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
